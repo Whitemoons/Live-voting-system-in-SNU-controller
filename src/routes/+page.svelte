66785a2fecf7148a23b415e24afcabe373668804
Whitemoons <alrnd01@snu.ac.kr>
2025-05-04 00:39:32 +0900

    fix: change label

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { setSavedState } from '$lib/state';
 
-    const labels = ['Round 1', 'Round 2~5', 'Round 6'];
+    const labels = ['Round 2~5', 'Round 6'];
     let selected = 9;
 
     function toggle(index: number) {
@@ -9,35 +9,44 @@
     }
 
     async function confirm() {
-        console.log("✅ 선택된 상태:", selected);
+        console.log("선택된 상태:", selected);
         setSavedState(selected);
-        toggle(9);
+        toggle(7);
     }
 
     function buttonLabel(i: number) {
-        if ([0, 2, 4, 6].includes(i)) return '히스';
-        if ([1, 3, 5, 7].includes(i)) return '혼또니';
-        if (i === 8) return 'Final';
+        if (i == 0) return 'Inital';
+        if ([2, 4].includes(i)) return '히스';
+        if ([1, 3].includes(i)) return '혼또니';
+        if (i === 5) return 'Final';
+        if (i == 6) return 'result';
         return '확인';
     }
 </script>
 
 <div class="grid">
+    <div class="label">Round 1</div>
+    <button class="button  {selected == 0 ? 'selected' : ''}" on:click={() => toggle(0)}>
+        {buttonLabel(0)}
+    </button>
+    <button class="button" on:click={() => confirm()}>
+        {buttonLabel(7)}
+    </button>
     {#each labels as label, rowIdx}
         <div class="label">{label}</div>
-        <button class="button {selected ==  rowIdx * 2 ? 'selected' : ''}" on:click={() => toggle(rowIdx * 2)}>
-            {buttonLabel(rowIdx * 2)}
-        </button>
         <button class="button {selected ==  rowIdx * 2 + 1 ? 'selected' : ''}" on:click={() => toggle(rowIdx * 2 + 1)}>
             {buttonLabel(rowIdx * 2 + 1)}
         </button>
+        <button class="button {selected ==  rowIdx * 2 + 2 ? 'selected' : ''}" on:click={() => toggle(rowIdx * 2 + 2)}>
+            {buttonLabel(rowIdx * 2 + 2)}
+        </button>
     {/each}
     <div></div>
-    <button class="button  {selected == 8 ? 'selected' : ''}" on:click={() => toggle(8)}>
-        {buttonLabel(8)}
+    <button class="button  {selected == 5 ? 'selected' : ''}" on:click={() => toggle(5)}>
+        {buttonLabel(5)}
     </button>
-    <button class="button" on:click={() => confirm()}>
-        {buttonLabel(9)}
+    <button class="button  {selected == 6 ? 'selected' : ''}" on:click={() => toggle(6)}>
+        {buttonLabel(6)}
     </button>
 </div>
 
